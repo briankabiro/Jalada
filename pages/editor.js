6f728b5ae5402fb1b133e21ea0d55a30a5e1d77d
BrianLee <briankigondu@gmail.com>
2017-04-13 19:13:19 +0300

    editor page

--- pages/editor.js
+++ pages/editor.js
@@ -1,15 +1,8 @@
-import React from 'react';
-import {Editor, EditorState} from 'draft-js';
+import MyEditor from '../components/editor'
 
-export default class MyEditor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {editorState: EditorState.createEmpty()};
-    this.onChange = (editorState) => this.setState({editorState});
-  }
-  render() {
-    return (
-        <Editor editorState={this.state.editorState} onChange={this.onChange} />
-    );
-  }
-}
+export default Editor () => (
+	<div>
+		<p>Write your story here</p>
+		<MyEditor />
+	</div>
+)
