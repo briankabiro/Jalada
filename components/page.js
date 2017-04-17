21b87c9bb807a96c5a946e3ec0fe225752bf91dd
BrianLee <briankigondu@gmail.com>
2017-04-17 19:35:34 +0300

    new stuff

--- components/page.js
+++ components/page.js
@@ -1,7 +1,16 @@
 //header, meta, nav, page js
 import Header from './header'
 import Meta from './meta'
+/*
+	Get posts and display them on frontpage
+	when clicked pass the post data as props
+	use the props to display the reading page
 
+	Build draft js page and style it
+	connect the page to database so that it is styled
+	style about page
+
+*/
 export default ({ children }) => (
 	<div className="main">
 		<Meta />
